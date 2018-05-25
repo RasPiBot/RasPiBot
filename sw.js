2a7f6e364ba26860f05fd28b244429203f8e953b
RasPiBot <Shany.Carle@gmail.com>
2018-05-25 19:05:19 +0000

    Ajout du fetch

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-// JavaScript File
+/*importScripts('/cache-polyfill.js');
+self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open('airhorner').then(function(cache) {
+return cache.addAll([
+'/',
+'/index.html',
+'/index.html?homescreen=1',
+'/?homescreen=1',
+'/files/styles/main.css',
+'/scripts/main.min.js',
+'/sounds/airhorn.mp3'
+]);
+})
+);
+});
+*/
+self.addEventListener('fetch', function(event) {
+console.log(event.request.url);
+});
