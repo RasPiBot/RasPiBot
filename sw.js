54840187d5d5fd23eafe02139998e956173d8e25
RasPiBot <Shany.Carle@gmail.com>
2018-05-25 19:10:48 +0000

    Reponse du service worker

--- sw.js
+++ sw.js
@@ -17,4 +17,9 @@ return cache.addAll([
 */
 self.addEventListener('fetch', function(event) {
 console.log(event.request.url);
+event.respondWith(
+caches.match(event.request).then(function(response) {
+return response || fetch(event.request);
+})
+);
 });
