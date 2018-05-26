48621e12b82df070d1bda97e318d0d22106d09d0
RasPiBot <Shany.Carle@gmail.com>
2018-05-26 00:29:25 +0000

    test git add

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
-/*importScripts('/cache-polyfill.js');
+//importScripts('/cache-polyfill.js');
 self.addEventListener('install', function(e) {
 e.waitUntil(
-caches.open('airhorner').then(function(cache) {
+caches.open('raspibot').then(function(cache) {
 return cache.addAll([
 '/',
 '/index.html',
 '/index.html?homescreen=1',
 '/?homescreen=1',
-'/files/styles/main.css',
-'/scripts/main.min.js',
-'/sounds/airhorn.mp3'
+'/files/css/style.css',
+'/files/js/xone-form.js'
 ]);
 })
 );
 });
-*/
+
 self.addEventListener('fetch', function(event) {
 console.log(event.request.url);
 event.respondWith(
