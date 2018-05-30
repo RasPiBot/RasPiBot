321e30419c0eea1e4dfea7e91898004adb509f57
RasPiBot <Shany.Carle@gmail.com>
2018-05-30 00:42:19 +0000

    Ajout du bouton add

--- sw.js
+++ sw.js
@@ -23,6 +23,15 @@ return response || fetch(event.request);
 );
 });
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
 window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   deferredPrompt = e;
